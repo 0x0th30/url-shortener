ad550434c8907eb3b7f8ddc7e2340ce8c4825d44
theo silva <thedorotheodore@gmail.com>
2022-11-14 23:35:45 -0300

    feature: include winston custom logger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { router } from '@routes/routes';
+import { logger } from '@utils/logger';
 
-const port = process.env.PORT || 3000;
+// eslint-disable-next-line dot-notation
+const port = process.env['PORT'] || 3000;
 const server = express();
 
 server.use(express.json());
 server.use(router);
 
 server.listen(port, () => {
-  console.log(`running at http://localhost:${port}/`);
+  logger.info(`Running application at http://localhost:${port}/`);
 });
---
+++ src/utils/logger.ts
@@ -0,0 +1,21 @@
+import moment from 'moment';
+import { createLogger, format, transports } from 'winston';
+import { reset, dim } from '@utils/ansi-style-codes';
+
+const logTemplate = format.printf(({
+  level, message, timestamp,
+}) => {
+  const formattedTimestamp = moment(timestamp).format('YYYY/MM/DD - hh:mm:ss');
+  const colorizedTimestamp = `${dim}${formattedTimestamp}${reset}`;
+  return `[${level}] | ${colorizedTimestamp} | ${message}`;
+});
+
+export const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.timestamp(),
+    format.colorize(),
+    logTemplate,
+  ),
+  transports: [new transports.Console()],
+});
