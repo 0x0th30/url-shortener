98c38712c218fd52f0bf0aadebc556c3636b00a9
theo silva <thedorotheodore@gmail.com>
2022-11-22 05:27:50 -0300

    feature(create-url-service): implements new service

---
+++ src/entities/url.ts
@@ -0,0 +1,52 @@
+import { PrismaClient } from '@prisma/client';
+// import { NotFoundUrl } from '@errors/application-errors';
+import { logger } from '@loaders/logger';
+
+export interface IUrl {
+  id: string,
+  url: string,
+}
+
+export class Url {
+  private prisma: PrismaClient;
+
+  constructor(prismaClient: PrismaClient) {
+    this.prisma = prismaClient;
+  }
+
+  public async searchById(id: string): Promise<IUrl | undefined> {
+    logger.info('Starting to search url by id...');
+
+    try {
+      const url = await this.prisma.url.findFirst({ where: { id } });
+      if (url) {
+        logger.info(`The url with id "${id}" was successfully found.`);
+        return url;
+      }
+    } catch (error: any) {
+      logger.error(`Searching was failed by ${error.name}!`);
+      throw error;
+    }
+
+    logger.info('The specified url id does not exists.');
+    return undefined;
+  }
+
+  public async create(data: IUrl): Promise<IUrl | undefined> {
+    logger.info('Starting to create url with the specified data...');
+
+    try {
+      const url = await this.prisma.url.create({ data });
+      if (url) {
+        logger.info('The url was successfully created.');
+        return url;
+      }
+    } catch (error: any) {
+      logger.error(`Creation was failed by ${error.name}!`);
+      throw error;
+    }
+
+    logger.error('The url cannot be created.');
+    return undefined;
+  }
+}
--- src/services/create-url-service.ts
+++ src/services/create-url-service.ts
@@ -1,23 +1,82 @@
 import md5 from 'md5';
+import { Url } from '@entities/url';
+import { prisma } from '@loaders/prisma-client';
+import { logger } from '@loaders/logger';
 
 interface CreateUrlRequest {
   url: string,
 }
 
+interface CreateUrlResponse {
+  success: boolean,
+  shortenedLink?: string,
+  message?: string,
+}
+
 class CreateUrlService {
-  public execute(request: CreateUrlRequest) {
-    const urlId = this.generateUrlId(request.url);
+  private url: Url;
+
+  constructor(url: Url) {
+    this.url = url;
+  }
+
+  public async execute(request: CreateUrlRequest): Promise<CreateUrlResponse> {
+    logger.info('Initializing create-url-service...');
+
+    const response: CreateUrlResponse = { success: false };
+
+    logger.info('Generating a url id...');
+    const id = this.generateUrlId(request.url);
+
+    logger.info('Checking if the requested url already be shortened...');
+    const urlExists = await this.url.searchById(id);
+    if (urlExists) {
+      response.success = true;
+      response.shortenedLink = urlExists.id;
+
+      return response;
+    }
+
+    logger.info('Creating a new register to the requested url...');
+    const createUrl = await this.url.create({ id, url: request.url });
+    if (createUrl) {
+      response.success = true;
+      response.shortenedLink = createUrl.id;
+
+      return response;
+    }
+
+    logger.error('The url cannot be shortened by unknown reasons!');
+
+    response.success = false;
+    response.message = 'The url cannot be shortened, please try again later!';
+
+    return response;
   }
 
   private generateUrlId(url: string) {
+    logger.info('Starting to generate url id...');
+
     const urlBuffer = Buffer.from(url);
 
     const base64Url = urlBuffer.toString('base64');
     const md5Url = md5(base64Url);
     const urlId = md5Url.slice(0, 6);
 
+    logger.info('The url id was successfully generated.');
+
     return urlId;
   }
 }
+
+(async () => {
+  const x = new Url(prisma);
+  const a = new CreateUrlService(x);
+  const request: CreateUrlRequest = {
+    url: 'https://www.prisma.io/docs/getting-started/aab',
+  };
+  const b = await a.execute(request);
+  console.log(b);
+})();
 
 export { CreateUrlService, CreateUrlRequest };
